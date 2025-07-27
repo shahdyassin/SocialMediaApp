03947e297ea7cb1df791f864cb60a0c58497c7e3
Shahd Yassin <shahdyassin63@gmail.com>
2025-07-28 00:56:59 +0300

    Prepared Working Environment

--- SocialMediaApp/Program.cs
+++ SocialMediaApp/Program.cs
@@ -8,14 +8,16 @@ namespace SocialMediaApp
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
-
+            builder.Services.AddSwaggerGen();
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.4
+                app.UseSwagger();
+                app.UseSwaggerUI();
                 app.UseHsts();
             }
 
